8d044c3d6f0f45c888ee839c587bc20489e14932
KGESH <seonghyeon.jee@gmail.com>
2022-12-28 19:16:20 +0900

    [Chore] init project

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: ['img.icons8.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
