33b05aa8c6bb15b38521dbe8716fb7831b1c440e
KGESH <seonghyeon.jee@gmail.com>
2022-12-29 17:31:42 +0900

    [Refactor] add post return type

--- libs/services/post/post.service.ts
+++ libs/services/post/post.service.ts
@@ -1,11 +1,12 @@
 import { prismaClient } from '../../prisma/prisma.client';
 import { Prisma } from '.prisma/client';
-import { PostsResponse } from '../../api-interfaces/response/post/post.interface';
+import { PostResponse, PostsResponse } from '../../api-interfaces/response/post/post.response';
+import { HTTP_STATUS } from '../../constants/http';
 import PostCreateArgs = Prisma.PostCreateArgs;
 
-export const createPost = async (createPostDto: PostCreateArgs) => {
+export const createPost = async (createPostDto: PostCreateArgs): Promise<PostResponse> => {
   const post = await prismaClient.post.create(createPostDto);
-  return post;
+  return { data: post, message: 'success', status: HTTP_STATUS.OK };
 };
 
 export const getPost = async (id: string) => {
@@ -22,5 +23,5 @@ export const getPosts = async (): Promise<PostsResponse> => {
   const posts = await prismaClient.post.findMany({
     include: { author: true, category: true },
   });
-  return { data: posts, message: 'success', status: 200 };
+  return { data: posts, message: 'success', status: HTTP_STATUS.OK };
 };
