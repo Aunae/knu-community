7f3296a2a31fe415c1d61e13b8dcf83c45152cfa
KGESH <seonghyeon.jee@gmail.com>
2022-12-29 17:33:24 +0900

    [Feat] HTTP constant string

---
+++ libs/constants/http.ts
@@ -0,0 +1,16 @@
+export const HTTP_METHOD = {
+  GET: 'GET',
+  POST: 'POST',
+};
+
+export type HTTP_METHOD = typeof HTTP_METHOD[keyof typeof HTTP_METHOD];
+
+export const HTTP_STATUS = {
+  OK: 200,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+};
+
+export type HTTP_STATUS = typeof HTTP_STATUS[keyof typeof HTTP_STATUS];
+
+export const APPLICATION_JSON = 'application/json';
