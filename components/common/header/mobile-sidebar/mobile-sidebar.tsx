3589f09d64ba8560faa49a839a4765b12517698f
KGESH <seonghyeon.jee@gmail.com>
2022-12-29 17:33:40 +0900

    [Feat] auth navbar

--- components/common/header/mobile-sidebar/mobile-sidebar.tsx
+++ components/common/header/mobile-sidebar/mobile-sidebar.tsx
@@ -1,8 +1,8 @@
 import Accordion from './accordion';
 import { CategoryWithChildren } from '../../../../libs/models/category';
+import Link from 'next/link';
 
 interface Props {
-  // children: ReactNode;
   categories: CategoryWithChildren[];
   open: Boolean;
   handleClose: () => void;
@@ -12,9 +12,19 @@ const MobileSidebar = ({ open, handleClose, categories }: Props) => {
   return (
     open && (
       <aside>
-        <section className="grid bg-gray-100 w-64 h-screen fixed left-0 top-0 z-20 ">
+        <section className="grid bg-gray-100 w-64 h-screen fixed left-0 top-0 z-20">
           <nav>
-            <ul className="px-4 py-4">
+            <div className="grid grid-cols-2 gap-2 justify-center text-center">
+              <Link href="/signup" onClick={handleClose} className="bg-red-400">
+                회원가입
+              </Link>
+              <Link href="/login" onClick={handleClose} className="bg-amber-200">
+                로그인
+              </Link>
+            </div>
+
+            {/*카테고리 목록*/}
+            <ul className="py-4">
               {categories.map((category) => (
                 <Accordion key={category.id} category={category} />
               ))}
