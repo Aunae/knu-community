bc822d53136c32fbcd5f1f4d31b7be76d5ee1624
KGESH <seonghyeon.jee@gmail.com>
2022-12-29 22:28:23 +0900

    [Feat] local dev db script

---
+++ utils/scripts/start-local-db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+SERVER="postgres_container";
+PW="example";
+DB="example";
+
+# Port 5433임. 5432랑 혼동 x
+echo "echo stop & remove old docker [$SERVER] and starting new fresh instance of [$SERVER]"
+(docker kill $SERVER || :) && \
+  (docker rm $SERVER || :) && \
+  docker run --name $SERVER -e POSTGRES_PASSWORD=$PW \
+  -e PGPASSWORD=$PW \
+  -p 5433:5432 \
+  -d postgres
+
+# wait for pg to start
+echo "sleep wait for pg-server [$SERVER] to start";
+SLEEP 3;
+
+# create the db
+echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | docker exec -i $SERVER psql -U postgres
+echo "\l" | docker exec -i $SERVER psql -U postgres
