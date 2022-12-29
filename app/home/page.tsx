4a2d43bf99b68916f0a4303a6db1f77b0e674989
KGESH <seonghyeon.jee@gmail.com>
2022-12-29 11:52:51 +0900

    [Feat] home post section

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,13 @@
-const HomePage = () => {
-  return <div>home page</div>;
+import PostSection from './post-section';
+
+const HomePage = async () => {
+  return (
+    <div>
+      <div>home page</div>
+      {/* @ts-expect-error Server Component */}
+      <PostSection />
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ app/home/post-section.tsx
@@ -0,0 +1,19 @@
+import { getMockPosts } from '../../libs/utils/mock-post';
+
+const PostSection = async () => {
+  const { data: posts } = await getMockPosts();
+
+  return (
+    <>
+      {posts?.map((post) => (
+        <div key={post.id}>
+          <p>{post.title}</p>
+          <p>{post.author.name}</p>
+          <p>{post.description}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default PostSection;
