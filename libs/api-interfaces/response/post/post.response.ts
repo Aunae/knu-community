b39cd9b31692c971baa83c17a6347666778e5b84
KGESH <seonghyeon.jee@gmail.com>
2022-12-29 17:33:58 +0900

    [Refactor] add post type

--- libs/api-interfaces/response/post/post.response.ts
+++ libs/api-interfaces/response/post/post.response.ts
@@ -1,5 +1,6 @@
 import { ResponseEntity } from '../response';
 import { PostWithCategory } from '../../../models/post';
+import { Post } from '@prisma/client';
 
-export type PostResponse = ResponseEntity<PostWithCategory>;
+export type PostResponse = ResponseEntity<Post | PostWithCategory>;
 export type PostsResponse = ResponseEntity<PostWithCategory[]>;
